8d738efdc8527d060218269c152a17f6e84a690f
SOFIA <sofiadamasin@gmail.com>
2025-04-14 16:11:50 -0300

    Update membro-academia.py

--- models/membro-academia.py
+++ models/membro-academia.py
@@ -1,5 +1,14 @@
 from pessoa import Pessoa
+from voto import Voto
 
 class MembroAcademia(Pessoa):
-    def __init__(self, id, nome, data_de_nascimento, nacionalidade):
+    def __init__(self, id: int, nome: str, data_de_nascimento: int, nacionalidade: str, votos: Voto):
         super().__init__(id, nome, data_de_nascimento, nacionalidade)
+        self.__votos = []
+
+    @property
+    def votos(self):
+        return self.__votos
+
+    def adicionar_voto(self, voto: Voto):
+        self.__votos.append(voto)
