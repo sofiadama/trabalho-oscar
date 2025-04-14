a4e1cd61ba06d344b73c25b0cec68223cbc23bfa
SOFIA <sofiadamasin@gmail.com>
2025-04-14 16:24:42 -0300

    Create filme.py

---
+++ models/filme.py
@@ -0,0 +1,40 @@
+from diretor import Diretor
+
+class Filme:
+    def __init__(self, titulo: str, ano: int, diretor: Diretor, categoria: Categoria):
+        self.__titulo = titulo
+        self.__ano_de_lancamento = ano_de_lancamento
+        self.__diretor = diretor
+        self.__categoria = categoria
+    
+    @property
+    def titulo(self):
+      return self.__titulo
+
+    @titulo.setter
+    def titulo(self, titulo):
+      self.__titulo = titulo
+
+    @property
+    def ano_de_lancamento(self):
+      return self.__ano_de_lancamento
+
+    @ano_de_lancamento.setter
+    def ano_de_lancamento(self, ano_de_lancamento):
+      self.__ano_de_lancamento = ano_de_lancamento
+      
+    @property
+    def diretor(self):
+      return self.__diretor
+
+    @diretor.setter
+    def diretor(self, diretor):
+      self.__diretor = diretor
+
+    @property
+    def categoria(self):
+      return self.__categoria
+
+    @categoria.setter
+    def categoria(self, categoria):
+      self.__categoria = categoria
