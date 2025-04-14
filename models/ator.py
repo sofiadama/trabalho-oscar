f4ac2ad78699c5e73d8a745d0e50f40a2df50f3e
SOFIA <sofiadamasin@gmail.com>
2025-04-14 16:14:52 -0300

    Update ator.py

--- models/ator.py
+++ models/ator.py
@@ -1,5 +1,5 @@
 from pessoa import Pessoa
 
 class Ator(Pessoa):
-    def __init__(self, id, nome, data_de_nascimento, nacionalidade):
+    def __init__(self, id: int, nome: str, data_de_nascimento: int, nacionalidade: str):
         super().__init__(id, nome, data_de_nascimento, nacionalidade)
