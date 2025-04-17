81e71ce08d98b8bf773a2f80924811e9cecc7c05
SOFIA <sofiadamasin@gmail.com>
2025-04-17 20:52:21 -0300

    Update filme.py

--- models/filme.py
+++ models/filme.py
@@ -2,40 +2,49 @@ from diretor import Diretor
 from categoria import Categoria
 
 class Filme:
-    def __init__(self, titulo: str, ano: int, diretor: Diretor, categoria: Categoria):
-        self.__titulo = titulo
-        self.__ano_de_lancamento = ano_de_lancamento
-        self.__diretor = diretor
-        self.__categoria = categoria
+    def __init__(self, titulo: str, ano: int, diretor: Diretor, ator: Ator, categoria: Categoria):
+        self.titulo = titulo
+        self.ano_de_lancamento = ano_de_lancamento
+        self.diretor = diretor
+        self.ator = ator
+        self.categoria = categoria
     
     @property
     def titulo(self):
-      return self.__titulo
+      return self.titulo
 
     @titulo.setter
     def titulo(self, titulo):
-      self.__titulo = titulo
+      self.titulo = titulo
 
     @property
     def ano_de_lancamento(self):
-      return self.__ano_de_lancamento
+      return self.ano_de_lancamento
 
     @ano_de_lancamento.setter
     def ano_de_lancamento(self, ano_de_lancamento):
-      self.__ano_de_lancamento = ano_de_lancamento
+      self.ano_de_lancamento = ano_de_lancamento
       
     @property
     def diretor(self):
-      return self.__diretor
+      return self.diretor
 
     @diretor.setter
     def diretor(self, diretor):
-      self.__diretor = diretor
+      self.diretor = diretor
+
+    @property
+    def ator(self):
+      return self.ator
+
+    @ator.setter
+    def ator(self, ator):
+      self.ator = ator
 
     @property
     def categoria(self):
-      return self.__categoria
+      return self.categoria
 
     @categoria.setter
     def categoria(self, categoria):
-      self.__categoria = categoria
+      self.categoria = categoria
