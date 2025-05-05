6b8b697ecf669f4cfd737d12f5d5e3f2d6bbbb1c
SOFIA <sofiadamasin@gmail.com>
2025-05-04 21:20:37 -0300

    Update voto.py

--- models/voto.py
+++ models/voto.py
@@ -1,40 +1,21 @@
 from membroAcademia import MembroAcademia
 
 class Voto:
-  def __init__(self, id: int, membro: MembroAcademia, indicado: Indicacao, ano_de_indicacao: int):
-    self.__id = id
+  def __init__(self, membro: MembroAcademia):
     self.__membro = membro
-    self.__indicado = indicado
-    self.__ano_de_indicacao = ano_de_indicacao
-
-  @property
-  def id(self):
-    return self.__id
-
-  @id.setter
-  def id(self, id):
-    self.__id = id
 
   @property
   def membro(self):
     return self.__membro
 
-  @membro.setter
-  def membro(self, membro):
-    self.__membro = membro
+  def inclui_voto():
+    pass
 
-  @property
-  def indicado(self):
-    return self.__indicado
+  def exclui_voto():
+    pass
 
-  @indicado.setter
-  def indicado(self, indicado):
-    self.__indicado = indicado
+  def altera_voto():
+    pass
 
-  @property
-  def ano_de_indicacao(self):
-    return self.__ano_de_indicacao
-
-  @indicacao.setter
-  def ano_de_indicacao(self, ano_de_indicacao):
-    self.__ano_de_indicacao = ano_de_indicacao
+  def lista_voto():
+    pass
