110026179b5b4418d9a3c479f0b64c15e6c8fb17
SOFIA <sofiadamasin@gmail.com>
2025-05-04 21:13:49 -0300

    Update diretor.py

--- models/diretor.py
+++ models/diretor.py
@@ -3,3 +3,4 @@ from pessoa import Pessoa
 class Diretor(Pessoa):
     def __init__(self, id: int, nome: str, data_de_nascimento: int, nacionalidade: str):
         super().__init__(id, nome, data_de_nascimento, nacionalidade)
+        self.__filmes_dirigidos = []
