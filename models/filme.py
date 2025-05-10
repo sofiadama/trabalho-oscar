06a0370191349a4bf3c1ed2eb4bfcfc009399469
SOFIA <sofiadamasin@gmail.com>
2025-05-10 15:42:09 -0300

    Update filme.py

--- models/filme.py
+++ models/filme.py
@@ -1,15 +1,11 @@
 from diretor import Diretor
-from ator import Ator
-from categoria import Categoria
 
 class Filme:
-    def __init__(self, titulo: str, ano: int, diretor: Diretor, ator: Ator, categoria: Categoria):
+    def __init__(self, titulo: str, diretor: Diretor, ano_de_lancamento: int):
         self.__titulo = titulo
-        self.__ano_de_lancamento = ano_de_lancamento
         self.__diretor = diretor
-        self.__ator = ator
-        self.__categoria = categoria
-    
+        self.__ano_de_lancamento = ano_de_lancamento
+        
     @property
     def titulo(self):
       return self.__titulo
@@ -19,14 +15,6 @@ class Filme:
       self.__titulo = titulo
 
     @property
-    def ano_de_lancamento(self):
-      return self.__ano_de_lancamento
-
-    @ano_de_lancamento.setter
-    def ano_de_lancamento(self, ano_de_lancamento):
-      self.__ano_de_lancamento = ano_de_lancamento
-      
-    @property
     def diretor(self):
       return self.__diretor
 
@@ -35,17 +23,9 @@ class Filme:
       self.__diretor = diretor
 
     @property
-    def ator(self):
-      return self.__ator
+    def ano_de_lancamento(self):
+      return self.__ano_de_lancamento
 
-    @ator.setter
-    def ator(self, ator):
-      self.__ator = ator
-
-    @property
-    def categoria(self):
-      return self.__categoria
-
-    @categoria.setter
-    def categoria(self, categoria):
-      self.__categoria = categoria
+    @ano_de_lancamento.setter
+    def ano_de_lancamento(self, ano_de_lancamento):
+      self.__ano_de_lancamento = ano_de_lancamento
