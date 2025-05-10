d5783d239c51911b91fd85fc195989695bf3e9c3
SOFIA <sofiadamasin@gmail.com>
2025-05-10 14:54:08 -0300

    Update ator.py

--- models/ator.py
+++ models/ator.py
@@ -3,4 +3,12 @@ from pessoa import Pessoa
 class Ator(Pessoa):
     def __init__(self, id: int, nome: str, data_de_nascimento: int, nacionalidade: str):
         super().__init__(id, nome, data_de_nascimento, nacionalidade)
-        self.__filmes_atuados = []
+        self.__atores_indicados = []
+
+    def indicar_ator():
+        for ator_indicado in self.__atores_indicados:
+            if novo_ator_indicado == ator_indicado:
+                return "Ator já foi indicado."
+
+        novo_ator_indicado = {"id": id, "nome": nome, "data_de_nascimento": data_de_nascimento, "nacionalidade": nacionalidade}
+        self.__atores_indicados.append(novo_ator_indicado)
