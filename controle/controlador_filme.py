2d82d10e109c34821fe103d842855e8f63b8e11d
SOFIA <sofiadamasin@gmail.com>
2025-05-19 22:43:40 -0300

    Update controlador_filme.py

--- controle/controlador_filme.py
+++ controle/controlador_filme.py
@@ -1,40 +1,86 @@
+from tela.tela_filme import TelaFilme
 from entidade.filme import Filme
-from entidade.diretor import Diretor
-from entidade.categoria import Categoria
 
-class ControladorFilme:
-    def __init__(self):
-        self.__filmes = []
-
-    def cadastrar_filme(self, titulo: str, diretor: Diretor, categoria: Categoria):
-        filme = Filme(titulo, diretor, categoria)
-        self.__filmes.append(filme)
-        return filme
-
-    def listar_filmes(self):
-        return self.__filmes
-
-    def buscar_filme_por_titulo(self, titulo: str):
-        for filme in self.__filmes:
+class ControladorFilme():
+    def __init__(self, controlador_sistema):
+        self.__filmes_indicados = []
+        self.__tela_filme = TelaFilme()
+        self.__controlador_sistema = controlador_sistema
+    
+    def pegar_filme_por_titulo(self, titulo: str):
+        for filme in self.__filmes_indicados:
             if filme.titulo == titulo:
                 return filme
+            elif self.__filmes_indicados.count(filme) > 1:
+                self.__tela_filme.mostrar_mensagem("Há mais de um filme com esse título.")
         return None
 
-    def remover_filme(self, titulo: str):
-        filme = self.buscar_filme_por_titulo(titulo)
-        if filme:
-            self.__filmes.remove(filme)
-            return True
-        return False
+    def adicionar_filme(self):
+        dados_filme = self.__tela_filme.pegar_dados_filme()
+        for filme in self.__filmes_indicados:
+            if filme.titulo == dados_filme["titulo"] and filme.sinopse == dados_filme["sinopse"]:
+                print("\nFilme já foi cadastrado.")
+                return
 
-    def alterar_filme(self, titulo_atual: str, novo_titulo: str = None, novo_diretor: Diretor = None, nova_categoria: Categoria = None):
-        filme = self.buscar_filme_por_titulo(titulo_atual)
-        if filme:
-            if novo_titulo:
-                filme.titulo = novo_titulo
-            if novo_diretor:
-                filme.diretor = novo_diretor
-            if nova_categoria:
-                filme.categoria = nova_categoria
-            return filme
-        return None
+        filme = Filme(
+            dados_filme["titulo"], 
+            dados_filme["sinopse"],
+            dados_filme["categoria"],
+            dados_filme["ano de indicacao"]
+        )
+
+        self.__filmes_indicados.append(filme)
+    
+    def alterar_dados(self):
+        titulo_filme = self.__tela_filme.buscar_filme()
+        filme = self.pegar_filme_por_titulo(titulo_filme)
+
+        if filme is not None:
+            novos_dados_filme = self.__tela_filme.pegar_dados_filme()
+            filme.titulo = novos_dados_filme["titulo"]
+            filme.sinopse = novos_dados_filme["sinopse"]
+            filme.categoria = novos_dados_filme["categoria"]
+            filme.ano_indicacao = novos_dados_filme["ano de indicacao"]
+            self.listar_filmes()
+
+        else:
+            self.__tela_filme.mostrar_mensagem("\nFilme não foi cadastrado.")
+
+    def listar_filmes(self):
+        print("----- FILMES INDICADOS -----\n")
+        for filme in self.__filmes_indicados:
+            self.__tela_filme.mostrar_dados_filme({
+                "titulo": filme.titulo, 
+                "sinopse": filme.sinopse,
+                "categoria": filme.categoria,
+                "ano de indicacao": filme.ano_indicacao 
+            })
+
+        if self.__filmes_indicados == []:
+            self.__tela_filme.mostrar_mensagem("\nNenhum filme indicado.")
+
+    def remover_filme(self):
+        titulo = self.__tela_filme.buscar_filme()
+        filme = self.pegar_filme_por_titulo(titulo)
+
+        if filme is not None:
+            self.__filmes_indicados.remove(filme)
+            self.__tela_filme.mostrar_mensagem("\nFilme removido com sucesso!")
+        else:
+            self.__tela_filme.mostrar_mensagem("\nFilme não foi indicado.")
+    
+    def retornar_menu(self):
+        self.__controlador_sistema.abrir_tela_indicacoes()
+    
+    def abrir_tela_filme(self):
+        opcoes = {
+            1: self.adicionar_filme, 
+            2: self.alterar_dados, 
+            3: self.listar_filmes, 
+            4: self.remover_filme, 
+            0: self.retornar_menu
+        }
+    
+        continuar = True
+        while continuar:
+            opcoes[self.__tela_filme.tela_opcoes()]()
