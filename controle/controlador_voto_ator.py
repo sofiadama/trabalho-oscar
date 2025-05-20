379798f46fc865450539f538f839ec5ffb649877
SOFIA <sofiadamasin@gmail.com>
2025-05-19 22:45:09 -0300

    Update controlador_voto_ator.py

--- controle/controlador_voto_ator.py
+++ controle/controlador_voto_ator.py
@@ -1,41 +1,89 @@
-from controle.controlador_voto import ControladorVoto
-from controle.controlador_ator import ControladorAtor
-from controle.controlador_membro import ControladorMembro
 from tela.tela_voto import TelaVoto
+from entidade.voto_ator import VotoAtor
+from controle.controlador_voto import ControladorVoto
 
 class ControladorVotoAtor(ControladorVoto):
     def __init__(self, controlador_sistema):
-        super().__init__()
+        super().__init__(self)
+        self.__votos_em_atores = []
+        self.__tela_voto = TelaVoto()
         self.__controlador_sistema = controlador_sistema
 
-    def autentificar_membro(self, membro):
-        membro = tela_voto.autentificacao_membro()
-        for membro in self.__membros_cadastrados:
-            if(membro.id != id):
-                print("Membro não cadastrado.")
+    def pegar_voto_por_categoria(self, categoria):
+        for voto in self.__votos_em_atores:
+            if voto.categoria == categoria:
+                return voto
+        return None
+
+    def adicionar_voto(self):
+        self.autenticar_membro(self.__controlador_membro.__membros)
+        dados_voto = self.__tela_voto.pegar_dados_voto()
+        for voto in self.__votos_em_atores:
+            if voto.membro == dados_voto["membro"] and voto.categoria == dados_voto["categoria"]:
+                print("Você já votou nesta categoria.")
                 return
-            else:
-                return
-        
-    def verificar_duplicidade_voto(self, id_membro, categoria):
-        dados_voto = self.__tela_voto.pega_dados_voto()
-        # membro_id acessa os votos do membro com id especifico
-        for voto in self.__votos[id_membro]:
-            if voto.categoria == dados_voto["categoria"]:
-                print("O membro/você já votou nesta categoria.")
-                return
+
+        voto = VotoAtor(
+            self.__membro_autenticado, 
+            dados_voto["indicado"],
+            dados_voto["categoria"],
+            dados_voto["ano"]
+        )
+
+        self.__votos_em_atores.append(voto)
+        super().pegar_lista_votos().append(voto)
     
-    def mostrar_atores_indicados():
-        indicados = ControladorAtor.listar_atores()
+    def alterar_voto(self):
+        categoria_voto = self.__tela_voto.buscar_voto()
+        voto = self.pegar_voto_por_categoria(categoria_voto)
 
-    def adicionar_voto():
-        pass
-
-    def alterar_voto():
-        pass
+        if voto is not None:
+            novos_dados_voto = self.__tela_voto.pegar_dados_voto()
+            voto.membro = novos_dados_voto["membro"]
+            voto.indicado = novos_dados_voto["indicado"]
+            voto.categoria = novos_dados_voto["categoria"]
+            voto.ano = novos_dados_voto["ano"]
     
-    def listar_votos():
-        pass
+            self.listar_votos_em_atores()
 
-    def remover_voto():
-        pass
+        else:
+            self.__tela_voto.mostrar_mensagem("\nVoto não foi registrado.")
+
+    def listar_votos_em_atores(self):
+        print("----- VOTOS EM ATORES -----\n")
+        for voto in self.__votos_em_atores:
+            self.__tela_voto.mostrar_dados_voto({
+                "membro": voto.membro, 
+                "indicado": voto.indicado,
+                "categoria": voto.categoria,
+                "ano": voto.ano
+            })
+
+        if self.__votos_em_atores == []:
+            self.__tela_voto.mostrar_mensagem("Nenhum voto em ator registrado.")
+
+    def remover_voto(self):
+        self.__tela_voto.buscar_voto()
+        voto = self.pegar_voto_por_categoria(voto)
+
+        if voto is not None:
+            self.__votos_em_atores.remove(voto)
+            self.__tela_voto.mostrar_mensagem("\nVoto removido com sucesso!")
+        else:
+            self.__tela_voto.mostrar_mensagem("\nVoto não foi registrado.")
+    
+    def retornar_menu(self):
+        self.__controlador_sistema.abrir_tela_votos()
+    
+    def abrir_tela_voto_ator(self):
+        opcoes = {
+            1: self.adicionar_voto, 
+            2: self.alterar_voto, 
+            3: self.listar_votos_em_atores, 
+            4: self.remover_voto, 
+            0: self.retornar_menu
+        }
+    
+        continuar = True
+        while continuar:
+            opcoes[self.__tela_voto.tela_votos_em_atores()]()
