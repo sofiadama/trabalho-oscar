7a13fc22a2c96d89ec84d337a39067a6825f4d78
SOFIA <sofiadamasin@gmail.com>
2025-05-19 22:42:21 -0300

    Update controlador_diretor.py

--- controle/controlador_diretor.py
+++ controle/controlador_diretor.py
@@ -1,79 +1,90 @@
-from tela_diretor import TelaDiretor
-from diretor import Diretor
+from tela.tela__diretor import TelaDiretor
+from entidade.diretor import Diretor
 
-class ControladorDiretor:
-   def __init__(self, controlador_sistema):
-        self.__diretores = []
+class ControladorDiretor():
+    def __init__(self, controlador_sistema):
+        self.__diretores_indicados = []
         self.__tela_diretor = TelaDiretor()
         self.__controlador_sistema = controlador_sistema
-
-    def buscar_diretor(self, nome: str):
-        for diretor in self.__diretores:
+    
+    def pegar_diretor_por_nome(self, nome: str):
+        for diretor in self.__diretores_indicados:
             if diretor.nome == nome:
                 return diretor
+            elif self.__diretores_indicados.count(diretor) > 1:
+                self.__tela_diretor.mostrar_mensagem("Há mais de um diretor com esse nome.")
         return None
 
     def adicionar_diretor(self):
         dados_diretor = self.__tela_diretor.pegar_dados_diretor()
-        diretor = Diretor(dados_diretor["nome"], dados_diretor["nacionalidade"], dados_diretor["filme"], dados_diretor["nacionalidade"]), dados_diretor["ano_indicacao"]
-        self.__diretores.append(diretor)
+        for diretor in self.__diretores_indicados:
+            if diretor.nome == dados_diretor["nome"] and diretor.filme == dados_diretor["filme"]:
+                print("\nDiretor já foi indicado.")
+                return
 
-    def alterar_diretor(self):
-        self.lista_diretores()
+        diretor = Diretor(
+            dados_diretor["nome"], 
+            dados_diretor["nacionalidade"],
+            dados_diretor["categoria"],
+            dados_diretor["filme"],
+            dados_diretor["ano de indicacao"]
+        )
+
+        self.__diretores_indicados.append(diretor)
+    
+    def alterar_dados(self):
         nome_diretor = self.__tela_diretor.buscar_diretor()
-        diretor = self.pega_diretor_por_nome(nome_diretor)
+        diretor = self.pegar_diretor_por_nome(nome_diretor)
 
         if diretor is not None:
-            novos_dados = self.__tela_diretor.buscar_diretor()
-            diretor.nome = novos_dados["nome"]
-            diretor.filme = novos_dados["filme"]
-            diretor.nacionalidade = novos_dados["nacionalidade"]
-            diretor.ano_indicacao = novos_dados["ano_indicacao"]
-            diretor.categoria = novos_dados["categoria"]
-            self.lista_diretores()
+            novos_dados_diretor = self.__tela_diretor.pegar_dados_diretor()
+            diretor.nome = novos_dados_diretor["nome"]
+            diretor.nacionalidade = novos_dados_diretor["nacionalidade"]
+            diretor.categoria = novos_dados_diretor["categoria"]
+            diretor.filme = novos_dados_diretor["filme"]
+            diretor.ano_indicacao = novos_dados_diretor["ano de indicacao"]
+            self.listar_diretores()
+
         else:
-            self.__tela_diretor.mostra_mensagem("ATENÇÃO: Diretor não encontrado.")
+            self.__tela_diretor.mostrar_mensagem("\nDiretor não foi indicado.")
 
     def listar_diretores(self):
-        if not self.__diretores:
-            self.__tela_diretor.mostrar_mensagem("Nenhum diretor cadastrado.")
-        for diretor in self.__diretores:
-            self.__tela_diretor.mostrar_diretor({
-                "nome": diretor.nome,
-                "categoria": diretor.categoria
-                "filme": diretor.filme
-                "ano_indicação": diretor.ano_indicação
-                "nacionalidade": diretor.nacionalidade
+        print("----- DIRETORES INDICADOS -----\n")
+        for diretor in self.__diretores_indicados:
+            self.__tela_diretor.mostrar_dados_diretor({
+                "nome": diretor.nome, 
+                "nacionalidade": diretor.nacionalidade,
+                "categoria": diretor.categoria,
+                "filme": diretor.filme,
+                "ano de indicacao": diretor.ano_indicacao 
             })
 
+        if self.__diretores_indicados == []:
+            self.__tela_diretor.mostrar_mensagem("Nenhum diretor indicado.")
+
     def remover_diretor(self):
-        self.lista_diretores()
-        id_diretor = self.__tela_diretor.selecionar_diretor()
-        diretor = self.pegar_diretor_por_id(id_diretor)
+        nome = self.__tela_diretor.buscar_diretor()
+        diretor = self.pegar_diretor_por_nome(nome)
 
         if diretor is not None:
-            self.__diretores.remover(diretor)
-            self.listar_diretores()
+            self.__diretores_indicados.remove(diretor)
+            self.__tela_diretor.mostrar_mensagem("\nDiretor removido com sucesso!")
         else:
-            self.__tela_diretor.mostrar_mensagem("ATENÇÃO: Diretor não encontrado.")
-
+            self.__tela_diretor.mostrar_mensagem("\nDiretor não foi indicado.")
+    
     def retornar_menu(self):
-        self.__controlador_sistema.abrir_tela()
-
-    def abrir_tela(self):
+        self.__controlador_sistema.abrir_tela_indicacoes()
+    
+    def abrir_tela_diretor(self):
         opcoes = {
-            1: self.incluir_diretor,
-            2: self.alterar_diretor,
-            3: self.lista_diretores,
-            4: self.excluir_diretor,
+            1: self.adicionar_diretor, 
+            2: self.alterar_dados, 
+            3: self.listar_diretores, 
+            4: self.remover_diretor, 
             0: self.retornar_menu
         }
-
-        while True:
-            opcao = self.__tela_diretor.tela_opcoes()
-            funcao_escolhida = opcoes.get(opcao)
-            if funcao_escolhida:
-                funcao_escolhida()
-            else:
-                self.__tela_diretor.mostrar_mensagem("Opção inválida.")
+    
+        continuar = True
+        while continuar:
+            opcoes[self.__tela_diretor.tela_opcoes()]()
 
