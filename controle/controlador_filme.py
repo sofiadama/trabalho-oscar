48e225e1f56cc49d964ac27f252803049e13f638
SOFIA <sofiadamasin@gmail.com>
2025-05-22 15:04:15 -0300

    Add files via upload

--- controle/controlador_filme.py
+++ controle/controlador_filme.py
@@ -1,26 +1,14 @@
 from tela.tela_filme import TelaFilme
 from entidade.filme import Filme
 
-class ControladorFilme():
+class ControladorFilme:
     def __init__(self, controlador_sistema):
         self.__filmes_indicados = []
         self.__tela_filme = TelaFilme()
         self.__controlador_sistema = controlador_sistema
-    
-    def pegar_filme_por_titulo(self, titulo: str):
-        for filme in self.__filmes_indicados:
-            if filme.titulo == titulo:
-                return filme
-            elif self.__filmes_indicados.count(filme) > 1:
-                self.__tela_filme.mostrar_mensagem("Há mais de um filme com esse título.")
-        return None
 
     def adicionar_filme(self):
         dados_filme = self.__tela_filme.pegar_dados_filme()
-        for filme in self.__filmes_indicados:
-            if filme.titulo == dados_filme["titulo"] and filme.sinopse == dados_filme["sinopse"]:
-                print("\nFilme já foi cadastrado.")
-                return
 
         filme = Filme(
             dados_filme["titulo"], 
@@ -29,24 +17,33 @@ class ControladorFilme():
             dados_filme["ano de indicacao"]
         )
 
+        if self.verificar_categoria_inexistente(dados_filme):
+            return
+        if self.verificar_categoria_irrestrita(dados_filme):
+            return
+        if self.verificar_duplicidade_indicacao(dados_filme):
+            return
+
         self.__filmes_indicados.append(filme)
-    
+
     def alterar_dados(self):
-        titulo_filme = self.__tela_filme.buscar_filme()
+        titulo_filme = self.__tela_filme.buscar_filme_por_titulo()
         filme = self.pegar_filme_por_titulo(titulo_filme)
 
-        if filme is not None:
-            novos_dados_filme = self.__tela_filme.pegar_dados_filme()
-            filme.titulo = novos_dados_filme["titulo"]
-            filme.sinopse = novos_dados_filme["sinopse"]
-            filme.categoria = novos_dados_filme["categoria"]
-            filme.ano_indicacao = novos_dados_filme["ano de indicacao"]
-            self.listar_filmes()
-
-        else:
+        if filme is None:
             self.__tela_filme.mostrar_mensagem("\nFilme não foi cadastrado.")
-
+            return
+        
+        novos_dados_filme = self.__tela_filme.pegar_dados_filme()
+        filme.titulo = novos_dados_filme["titulo"]
+        filme.sinopse = novos_dados_filme["sinopse"]
+        filme.categoria = novos_dados_filme["categoria"]
+        filme.ano_indicacao = novos_dados_filme["ano de indicacao"]
+        self.listar_filmes()
+        
     def listar_filmes(self):
+        self.mostrar_indicados_por_filtro()
+
         print("----- FILMES INDICADOS -----\n")
         for filme in self.__filmes_indicados:
             self.__tela_filme.mostrar_dados_filme({
@@ -56,21 +53,80 @@ class ControladorFilme():
                 "ano de indicacao": filme.ano_indicacao 
             })
 
-        if self.__filmes_indicados == []:
-            self.__tela_filme.mostrar_mensagem("\nNenhum filme indicado.")
-
     def remover_filme(self):
-        titulo = self.__tela_filme.buscar_filme()
+        titulo = self.__tela_filme.buscar_filme_por_titulo()
         filme = self.pegar_filme_por_titulo(titulo)
 
-        if filme is not None:
-            self.__filmes_indicados.remove(filme)
-            self.__tela_filme.mostrar_mensagem("\nFilme removido com sucesso!")
-        else:
+        if filme is None:
             self.__tela_filme.mostrar_mensagem("\nFilme não foi indicado.")
+
+        self.__filmes_indicados.remove(filme)
+        self.__tela_filme.mostrar_mensagem("\nFilme removido com sucesso!")
+
+    def verificar_categoria_inexistente(self, dados_filme):
+        categorias_cadastradas = self.__controlador_sistema.controlador_categoria.pegar_categorias_cadastradas()
+
+        if dados_filme["categoria"] not in [categoria.titulo for categoria in categorias_cadastradas]:
+            print("\nCategoria não cadastrada.")
+            return True
+        return False
+    
+    def verificar_categoria_irrestrita(self, categoria):
+        if categoria in self.__controlador_sistema.controlador_categoria.pegar_categorias_restritas():
+            print("\nCategoria restrita a atores e diretores.")
+            return True
+        return False
+    
+    def verificar_duplicidade_indicacao(self, dados_filme):
+        for filme in self.__filmes_indicados:
+            if filme.titulo == dados_filme["titulo"] and filme.sinopse == dados_filme["sinopse"]:
+                print("\nFilme já foi indicado.")
+                return True
+        return False
+
+    def pegar_filme_por_titulo(self, titulo: str):
+        for filme in self.__filmes_indicados:
+            if filme.titulo == titulo:
+                return filme
+            elif self.__filmes_indicados.count(filme) > 1:
+                self.__tela_filme.mostrar_mensagem("Há mais de um filme com esse título.")
+        return None
+    
+    def pegar_filme_por_categoria(self, categoria: str):
+        return [filme for filme in self.__filmes_indicados if filme.categoria == categoria]
+    
+    def pegar_filme_por_ano(self, ano_indicacao: int):
+        return [filme for filme in self.__filmes_indicados if filme.ano_indicacao == ano_indicacao]
+    
+    def filtrar_indicados_por_categoria(self):
+        categoria = self.__tela_filme.buscar_categoria()
+        filmes_filtrados = self.pegar_filme_por_categoria(categoria) 
+    
+        if not filmes_filtrados:
+            print(f"\nNenhum filme indicado na categoria '{categoria}'.")
+            return
+
+        print(f"----- FILMES INDICADOS NA CATEGORIA '{categoria}' -----\n")
+        for filme in filmes_filtrados:
+            print(filme.titulo)
+
+    def filtrar_indicados_por_ano(self):
+        ano_indicacao = self.__tela_filme.buscar_ano_indicacao()
+        filmes_filtrados = self.pegar_filme_por_ano(ano_indicacao) 
+
+        if not filmes_filtrados:
+            print(f"\nNenhum filme indicado no ano de '{ano_indicacao}'.")
+            return
+
+        print(f"----- FILMES INDICADOS NO ANO DE '{ano_indicacao}' -----\n")
+        for filme in filmes_filtrados:
+            print(filme.titulo)
+    
+    def pegar_filmes_indicados(self):
+        return self.__filmes_indicados
     
     def retornar_menu(self):
-        self.__controlador_sistema.abrir_tela_indicacoes()
+        self.__controlador_sistema.abrir_submenu_indicacoes()
     
     def abrir_tela_filme(self):
         opcoes = {
@@ -84,3 +140,14 @@ class ControladorFilme():
         continuar = True
         while continuar:
             opcoes[self.__tela_filme.tela_opcoes()]()
+    
+    def mostrar_indicados_por_filtro(self):
+        opcoes = {
+            1: self.filtrar_indicados_por_categoria,
+            2: self.filtrar_indicados_por_ano,
+            0: self.abrir_tela_filme
+        }
+
+        continuar = True
+        while continuar:
+            opcoes[self.__tela_filme.tela_opcoes_filtros()]()
