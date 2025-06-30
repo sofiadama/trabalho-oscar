cba6336dfb7ae48d324a2c56c488a38857e9b9a9
SOFIA <sofiadamasin@gmail.com>
2025-06-30 00:25:23 -0300

    Add files via upload

---
+++ excecoes/id_nao_encontrado_exception.py
@@ -0,0 +1,3 @@
+class IDNaoEncontradoException(Exception):
+    def __init__(self, mensagem):
+        super().__init__(mensagem)
---
+++ excecoes/voto_duplicado_exception.py
@@ -0,0 +1,3 @@
+class VotoDuplicadoException(Exception):
+    def __init__(self, mensagem):
+        super().__init__(mensagem)
